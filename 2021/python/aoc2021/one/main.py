c2464da82f45c24b7ad5c0333e6a00e1e614e53b
David Creemer <david@zachary.com>
2021-12-04 18:33:03 -0800

    add basic repo structure

---
+++ 2021/python/aoc2021/one/main.py
@@ -0,0 +1,16 @@
+# main program for day one
+
+def part_one() -> None:
+    """
+    part one of day one
+    """
+    return None
+
+
+def main() -> None:
+    """
+    main function for day one
+    """
+    print("Day One")
+    print("Part One:")
+    print("Part Two:")
---
+++ 2021/python/aoc2021/one/tests/test_foo.py
@@ -0,0 +1,7 @@
+# unittest for the calculate module
+
+from ..main import part_one
+
+
+def test_basic() -> None:
+    assert part_one() is None
