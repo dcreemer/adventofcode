8a096aed71a7e0a7082f97f6644d2f84bbd9a31f
David Creemer <david@zachary.com>
2021-12-04 19:59:20 -0800

    day 1

--- 2021/python/aoc2021/one/one.py
+++ 2021/python/aoc2021/one/one.py
@@ -1,16 +1,47 @@
-# main program for day one
+# AoC 2021 day one
 
-def part_one() -> int:
-    """
-    part one of day one
-    """
-    return 1
+import collections
+from itertools import islice
+from typing import Any, Generator, Iterable, Tuple
+
+
+def parse_input(input: str) -> list[int]:
+    return [int(n) for n in input.splitlines() if n]
+
+
+def sliding_window(iterable: Iterable[Any], n: int) -> Generator[Tuple[Any, ...], None, None]:
+    # from https://docs.python.org/3/library/itertools.html
+    # sliding_window('ABCDEFG', 4) -> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+
+def count_increases(depths: list[int]) -> int:
+    return sum([1 if b > a else 0 for a, b in zip(depths, depths[1:])])
+
+
+def part_one_calc(depths: list[int]) -> int:
+    return count_increases(depths)
+
+
+def part_two_calc(depths: list[int]) -> int:
+    depths = [sum(d) for d in sliding_window(depths, 3)]
+    return count_increases(depths)
 
 
 def main() -> None:
-    """
-    main function for day one
-    """
     print("Day One")
-    print("Part One:")
-    print("Part Two:")
+    with open("./aoc2021/one/input.txt", "r") as f:
+        data = f.read()
+    depths = parse_input(data)
+    print("Part One:", part_one_calc(depths))
+    print("Part Two:", part_two_calc(depths))
+
+
+if __name__ == "__main__":
+    main()
--- 2021/python/aoc2021/one/tests/test_one.py
+++ 2021/python/aoc2021/one/tests/test_one.py
@@ -1,7 +1,33 @@
 # tests for day one
 
-from ..one import part_one
+from ..one import parse_input, part_one_calc, part_two_calc, sliding_window
+
+input = """199
+200
+208
+210
+200
+207
+240
+260
+269
+263"""
 
 
-def test_basic() -> None:
-    assert part_one() == 1
+def test_parse_input() -> None:
+    x = parse_input(input)
+    assert len(x) == 10
+
+
+def test_sliding_window() -> None:
+    assert ["".join(x) for x in sliding_window("ABCDEFG", 4)] == ["ABCD", "BCDE", "CDEF", "DEFG"]
+
+
+def test_part_one() -> None:
+    depths = parse_input(input)
+    assert part_one_calc(depths) == 7
+
+
+def test_part_two() -> None:
+    depths = parse_input(input)
+    assert part_two_calc(depths) == 5
