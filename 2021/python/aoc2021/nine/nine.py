0f6521fe54ace43f341e976b2b320e216a412110
David Creemer <david@zachary.com>
2021-12-08 21:46:54 -0800

    finish day 9

--- 2021/python/aoc2021/nine/nine.py
+++ 2021/python/aoc2021/nine/nine.py
@@ -1,18 +1,60 @@
 # AoC 2021 day nine
 
+from functools import reduce
+from operator import mul
+from typing import Tuple
+
 from ..lib.input import read_raw_input_data
 
 
-def parse_input(input: str) -> list[str]:
-    return list()
+def parse_input(input: str) -> list[list[int]]:
+    return [[int(c) for c in line] for line in input.splitlines() if line]
 
 
-def part_one_calc(data: list[str]) -> int:
-    return 1
+def neighbors(x: int, y: int, data: list[list[int]]) -> list[Tuple[int, int]]:
+    result = []
+    if x > 0:
+        result.append((x-1, y))
+    if x < len(data[y])-1:
+        result.append((x+1, y))
+    if y > 0:
+        result.append((x, y-1))
+    if y < len(data)-1:
+        result.append((x, y+1))
+    return result
 
 
-def part_two_calc(data: list[str]) -> int:
-    return 2
+def low_points(data: list[list[int]]) -> list[Tuple[int, int]]:
+    low_points = []
+    for y in range(len(data)):
+        for x in range(len(data[y])):
+            if data[y][x] < min(data[ny][nx] for (nx, ny) in neighbors(x, y, data)):
+                low_points.append((x, y))
+    return low_points
+
+
+def part_one_calc(data: list[list[int]]) -> int:
+    low_point_values = low_points(data)
+    return sum([data[y][x] + 1 for (x, y) in low_point_values])
+
+
+def basin_size(data: list[list[int]], x: int, y: int) -> int:
+    visited = set()
+    to_visit = [(x, y)]
+    while to_visit:
+        x, y = to_visit.pop()
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
+        for (nx, ny) in neighbors(x, y, data):
+            if data[ny][nx] < 9:
+                to_visit.append((nx, ny))
+    return len(visited)
+
+
+def part_two_calc(data: list[list[int]]) -> int:
+    lps = low_points(data)
+    return reduce(mul, sorted([basin_size(data, x, y) for (x, y) in lps])[-3:], 1)
 
 
 def main() -> None:
--- 2021/python/aoc2021/nine/tests/test_nine.py
+++ 2021/python/aoc2021/nine/tests/test_nine.py
@@ -3,19 +3,25 @@
 from ..nine import parse_input, part_one_calc, part_two_calc
 
 input = """
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
 """
 
 
 def test_parse_input() -> None:
     data = parse_input(input)
-    assert len(data) == 0
+    assert len(data) == 5
+    assert data[0] == [2, 1, 9, 9, 9, 4, 3, 2, 1, 0]
 
 
 def test_part_one() -> None:
     data = parse_input(input)
-    assert part_one_calc(data) == 1
+    assert part_one_calc(data) == 15
 
 
 def test_part_two() -> None:
     data = parse_input(input)
-    assert part_two_calc(data) == 2
+    assert part_two_calc(data) == 1134
