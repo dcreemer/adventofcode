4ca6925bd17d1394f5000dbdfacc59e31b9f6963
David Creemer <david@zachary.com>
2021-12-10 15:43:36 -0800

    stage day 11

---
+++ 2021/python/aoc2021/eleven/eleven.py
@@ -0,0 +1,27 @@
+# AoC 2021 day eleven
+
+from ..lib.input import read_raw_input_data
+
+
+def parse_input(input: str) -> list[str]:
+    return [line for line in input.splitlines() if line]
+
+
+def part_one_calc(data: list[str]) -> int:
+    return 1
+
+
+def part_two_calc(data: list[str]) -> int:
+    return 2
+
+
+def main() -> None:
+    print("Day Eleven")
+    data = read_raw_input_data(__file__)
+    stuff = parse_input(data)
+    print("Part One:", part_one_calc(stuff))
+    print("Part Two:", part_two_calc(stuff))
+
+
+if __name__ == "__main__":
+    main()
