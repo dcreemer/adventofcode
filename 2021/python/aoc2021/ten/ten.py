4fd55ea31900f2364701a4d5caecc8bacc450555
David Creemer <david@zachary.com>
2021-12-09 21:28:57 -0800

    day ten

--- 2021/python/aoc2021/ten/ten.py
+++ 2021/python/aoc2021/ten/ten.py
@@ -1,6 +1,41 @@
 # AoC 2021 day ten
 
+from typing import Union
+
 from ..lib.input import read_raw_input_data
+
+CValues = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137,
+}
+
+IValues = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4,
+}
+
+
+def check_syntax(data: str) -> Union[str, list[str]]:
+    stack = []
+    for char in data:
+        # print(char, stack)
+        if char in '({[<':
+            stack.append(char)
+        elif char == ')' and stack[-1] != '(':
+            return ')'
+        elif char == ']' and stack[-1] != '[':
+            return ']'
+        elif char == '}' and stack[-1] != '{':
+            return '}'
+        elif char == '>' and stack[-1] != '<':
+            return '>'
+        else:
+            stack.pop()
+    return stack
 
 
 def parse_input(input: str) -> list[str]:
@@ -8,11 +43,25 @@ def parse_input(input: str) -> list[str]:
 
 
 def part_one_calc(data: list[str]) -> int:
-    return 1
+    result = 0
+    for line in data:
+        score = check_syntax(line)
+        if isinstance(score, str):
+            result += CValues[score]
+    return result
 
 
 def part_two_calc(data: list[str]) -> int:
-    return 2
+    results: list[int] = []
+    for line in data:
+        rest = check_syntax(line)
+        if isinstance(rest, list):
+            result = 0
+            for c in reversed(rest):
+                result *= 5
+                result += IValues[c]
+            results.append(result)
+    return sorted(results)[len(results) // 2]
 
 
 def main() -> None:
--- 2021/python/aoc2021/ten/tests/test_ten.py
+++ 2021/python/aoc2021/ten/tests/test_ten.py
@@ -3,24 +3,29 @@
 from ..ten import parse_input, part_one_calc, part_two_calc
 
 input = """
-2199943210
-3987894921
-9856789892
-8767896789
-9899965678
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
 """
 
 
 def test_parse_input() -> None:
     data = parse_input(input)
-    assert len(data) == 5
+    assert len(data) == 10
 
 
 def test_part_one() -> None:
     data = parse_input(input)
-    assert part_one_calc(data) == 1
+    assert part_one_calc(data) == 26397
 
 
 def test_part_two() -> None:
     data = parse_input(input)
-    assert part_two_calc(data) == 2
+    assert part_two_calc(data) == 288957
