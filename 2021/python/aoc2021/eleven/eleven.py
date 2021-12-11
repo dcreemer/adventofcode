3cd5e56e912072514b16289ccd9fb85c41a00f75
David Creemer <david@zachary.com>
2021-12-10 21:44:26 -0800

    complete day 11

--- 2021/python/aoc2021/eleven/eleven.py
+++ 2021/python/aoc2021/eleven/eleven.py
@@ -1,26 +1,75 @@
 # AoC 2021 day eleven
 
+from typing import Tuple
+
 from ..lib.input import read_raw_input_data
 
 
-def parse_input(input: str) -> list[str]:
-    return [line for line in input.splitlines() if line]
+def parse_input(input: str) -> list[list[int]]:
+    return [[int(x) for x in list(line)] for line in input.splitlines() if line]
 
 
-def part_one_calc(data: list[str]) -> int:
-    return 1
+def neighbors(x: int, y: int, data: list[list[int]]) -> list[Tuple[int, int]]:
+    result = []
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if dx == 0 and dy == 0:
+                continue
+            if x + dx >= 0 and x + dx < len(data[0]) and y + dy >= 0 and y + dy < len(data):
+                result.append((x + dx, y + dy))
+    return result
 
 
-def part_two_calc(data: list[str]) -> int:
-    return 2
+def flash(data: list[list[int]]) -> int:
+    # flash elements that are at 10, and update neighbors
+    # return number of flashes
+    flashes = 0
+    for y in range(len(data)):
+        for x in range(len(data[0])):
+            if data[y][x] >= 10:
+                data[y][x] = 0
+                flashes += 1
+                for nx, ny in neighbors(x, y, data):
+                    if data[ny][nx] > 0:
+                        data[ny][nx] += 1
+    return flashes
+
+
+def step(data: list[list[int]]) -> int:
+    # increase every element by 1
+    for y in range(len(data)):
+        for x in range(len(data[0])):
+            data[y][x] += 1
+    # check for flashes. keep going until no more flashes
+    flashes = 0
+    new_flashes = 1
+    while new_flashes > 0:
+        new_flashes = flash(data)
+        flashes += new_flashes
+    return flashes
+
+
+def part_one_calc(data: list[list[int]]) -> int:
+    return sum(step(data) for n in range(100))
+
+
+def all_zero(data: list[list[int]]) -> int:
+    return sum(sum(line) for line in data) == 0
+
+
+def part_two_calc(data: list[list[int]]) -> int:
+    n = 0
+    while not all_zero(data):
+        n += 1
+        step(data)
+    return n
 
 
 def main() -> None:
     print("Day Eleven")
     data = read_raw_input_data(__file__)
-    stuff = parse_input(data)
-    print("Part One:", part_one_calc(stuff))
-    print("Part Two:", part_two_calc(stuff))
+    print("Part One:", part_one_calc(parse_input(data)))
+    print("Part Two:", part_two_calc(parse_input(data)))
 
 
 if __name__ == "__main__":
--- 2021/python/aoc2021/eleven/tests/test_eleven.py
+++ 2021/python/aoc2021/eleven/tests/test_eleven.py
@@ -3,19 +3,31 @@
 from ..eleven import parse_input, part_one_calc, part_two_calc
 
 input = """
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
 """
 
 
 def test_parse_input() -> None:
     data = parse_input(input)
-    assert len(data) == 0
+    assert len(data) == 10
+    assert data[0][0] == 5
+    assert data[9][9] == 6
 
 
 def test_part_one() -> None:
     data = parse_input(input)
-    assert part_one_calc(data) == 1
+    assert part_one_calc(data) == 1656
 
 
 def test_part_two() -> None:
     data = parse_input(input)
-    assert part_two_calc(data) == 2
+    assert part_two_calc(data) == 195
